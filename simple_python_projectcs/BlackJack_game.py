8cdaecf9e873841846eeabb4828945d41fdb1389
SOHAN D P <sohandp123@gmail.com>
2023-06-17 01:04:36 +0530

    class Deck() created

--- simple_python_projectcs/BlackJack_game.py
+++ simple_python_projectcs/BlackJack_game.py
@@ -1,20 +1,38 @@
 import random
-cards = []
 
-suits = [ "Hearts", "Diamonds", "Spades", "Clubs" ]
-ranks = [ {"rank":"A","value":"1"},{"rank":"2","value":"2"},{"rank":"3","value":"3"},{"rank":"4","value":"4"}, {"rank":"5","value":"5"},{"rank":"6","value":"6"}, {"rank":"7","value":"7"},{"rank":"8","value":"8"}, {"rank":"9","value":"9"},{"rank":"10","value":"10"}, {"rank":"Q","value":"10"},{"rank":"K","value":"10"}, {"rank":"J","value":"10"} ]
+class Deck:
+  def __init__(self):
+
+    self.cards = []
+
+    suits = [ "Hearts", "Diamonds", "Spades", "Clubs" ]
+    ranks = [ {"rank":"A","value":"1"},{"rank":"2","value":"2"},{"rank":"3","value":"3"},{"rank":"4","value":"4"}, {"rank":"5","value":"5"},{"rank":"6","value":"6"}, {"rank":"7","value":"7"},{"rank":"8","value":"8"}, {"rank":"9","value":"9"},{"rank":"10","value":"10"}, {"rank":"Q","value":"10"},{"rank":"K","value":"10"}, {"rank":"J","value":"10"} ]
 
 
 
-for suit in suits:
-  for rank in ranks:
-    cards.append([suit,rank])
+    for suit in suits:
+      for rank in ranks:
+        self.cards.append([suit,rank])
 
-def shuffle():
-  random.shuffle(cards)
 
-shuffle()
-card = cards.pop()
-print(card)
-   
- 
+  def shuffle(self):
+    if(len(self.cards)>1):
+      random.shuffle(self.cards)
+
+  def deal(self,number):
+    cards_dealt = []
+    for x in range(number):
+      if(len(self.cards)>0):
+        card = self.cards.pop()
+        cards_dealt.append(card)
+
+    return cards_dealt
+
+deck=Deck()
+deck.shuffle()
+# card = deal(1)[0]  
+cards_dealt = deck.deal(1) 
+card = cards_dealt[0]
+
+
+print(card[1]["rank"],card[1]["value"])
