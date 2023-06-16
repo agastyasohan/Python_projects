a41fc6fbdfffe8f6261b0b044c591b89ef155c99
SOHAN D P <sohandp123@gmail.com>
2023-06-16 20:34:58 +0530

    user validation and play again option

--- simple_python_projectcs/Rock-Paper-Scissors_game.py
+++ simple_python_projectcs/Rock-Paper-Scissors_game.py
@@ -1,32 +1,43 @@
+
 import random
 
 def get_choices():
-	player_choice = input("Enter a choice (rock, paper, scissors):")
 	options = ["rock", "paper", "scissors"]
-	computer_choice = random.choice(options)
-	choices = {"player": player_choice, "computer": computer_choice}
-	return choices	
+
+	while True:
+		player_choice = input("Enter a choice (rock, paper, scissors):").lower()
+		if player_choice in options:
+			computer_choice = random.choice(options)
+			choices = {"player": player_choice, "computer": computer_choice}
+			return choices	
+		else:
+			print("Invalid Choice!. Please Try again:")
 	
 
 def check_win(player,computer):
-	print(f"you chose {player}, computer chose {computer}")
+	print(f"you chose {player}, computer chose {computer}\n")
 	
 	if player == computer:
-		return "It's a tie!"
+		return "'It's a tie!'"
 	elif player == "rock":
 		if computer == "paper":
-			return "Paper covers rock! You lose."
+			return "'Paper covers rock! You lose.'"
 		else:
-			return "Rock smashes scissors! You win!"
+			return "'Rock smashes scissors! You win!'"
 	elif player == "paper":
 		if computer == "rock":
-			return "Paper covers rock! You Win!"
+			return "'Paper covers rock! You Win!'"
 		else:
-			return "scissors cuts Paper! You lose."
+			return "'scissors cuts Paper! You lose.'"
 	
 
 # main function
-choices = get_choices()
-result = check_win(choices["player"], choices["computer"])
-print(result)
-	
+while True:
+	choices_dict = get_choices() # it is a dictionary that stores the choices made by player and computer.
+	result = check_win(choices_dict["player"], choices_dict["computer"])
+	print(result)
+	print()
+
+	play_again = input("Play again? (y/n)")
+	if play_again.lower() != "y":
+		break
